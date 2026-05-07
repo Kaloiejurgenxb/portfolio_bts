f7ded7573244a879d4d31f1b2b29876a8d066637
Kaloiejurgenxb <kaloiejurgen114@gmail.com>
2026-05-07 15:36:44 +1100

    Update TP1_ESP32.ino
    - Affiche l'adresse l'IP ESP32
    -Connecter la carte ESP32 à internet

--- TP1/TP1_ESP32.ino
+++ TP1/TP1_ESP32.ino
@@ -1,9 +1,18 @@
-void setup() {
-  Serial.begin(115200);
-}
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-void loop() {
-  int valeur = random(0, 100);
-  Serial.println(valeur);
-  delay(2000);
+const char* ssid = "VOTRE_WIFI";
+const char* password = "VOTRE_MOT_DE_PASSE";
+
+const char* serverName = "http://192.168.1.100/data.php";
+
+void setup() {
+
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
 }
